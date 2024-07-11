76144ce1507a05a490a95d5d8a5f92147a9865ad
Matthew Dring <matthewdring@icloud.com>
2024-07-11 19:13:41 +1200

    Add project files.

---
+++ VersionA-Task2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace VersionA_Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.rowAmountLabel = new System.Windows.Forms.Label();
+            this.rowAmountInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(600, 600);
+            this.pictureBoxDisplay.TabIndex = 2;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(649, 438);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(116, 23);
+            this.drawButton.TabIndex = 3;
+            this.drawButton.Text = "Draw Hand Bags";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(649, 467);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(116, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(649, 496);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(116, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // rowAmountLabel
+            // 
+            this.rowAmountLabel.AutoSize = true;
+            this.rowAmountLabel.Location = new System.Drawing.Point(662, 101);
+            this.rowAmountLabel.Name = "rowAmountLabel";
+            this.rowAmountLabel.Size = new System.Drawing.Size(91, 13);
+            this.rowAmountLabel.TabIndex = 6;
+            this.rowAmountLabel.Text = "Number Of Rows:";
+            // 
+            // rowAmountInput
+            // 
+            this.rowAmountInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rowAmountInput.Location = new System.Drawing.Point(649, 126);
+            this.rowAmountInput.Name = "rowAmountInput";
+            this.rowAmountInput.Size = new System.Drawing.Size(116, 20);
+            this.rowAmountInput.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 640);
+            this.Controls.Add(this.rowAmountInput);
+            this.Controls.Add(this.rowAmountLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label rowAmountLabel;
+        private System.Windows.Forms.TextBox rowAmountInput;
+    }
+}
+
---
+++ VersionA-Task2/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VersionA_Task2
+{
+    public partial class Form1 : Form
+    {
+        //Name: Matthew Dring   
+        //ID: 1264121
+
+        //Width of a hand bag
+        const int BAG_WIDTH = 50;
+        //Height of a hand bag
+        const int BAG_HEIGHT = 50;
+        //The number of columns of hand bags to draw
+        const int NUM_COLUMNS = 10;
+        //The gap between rows and columns
+        const int GAP = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method exits the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// This method clears the picturebox and textbox then sets the focus back to the textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            pictureBoxDisplay.Refresh();
+            rowAmountInput.Clear();
+            rowAmountInput.Focus();
+        }
+        /// <summary>
+        /// This is the click even method for the draw hand bags button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            int MINIMUM_ROWS = 2;
+            int MAXIMUM_ROWS = 10;
+            int numberOfRows = 0;
+
+            //Convert the input text into an integer
+            numberOfRows = Convert.ToInt32(rowAmountInput.Text);
+
+
+            //Check that the number of rows provided is valid otherwise return a error message
+            if (numberOfRows < MINIMUM_ROWS || numberOfRows > MAXIMUM_ROWS)
+            {
+                MessageBox.Show("Please enter a valid number between 2 and 10");
+                pictureBoxDisplay.Refresh();
+                rowAmountInput.Clear();
+                rowAmountInput.Focus();
+
+            }
+
+            //Create graphics object
+            Graphics g = pictureBoxDisplay.CreateGraphics();
+            //Clear the picturebox
+            pictureBoxDisplay.Refresh();
+
+            //Delcare variables
+            int startX = 10;
+            int startY = 10;
+
+
+            for (int row = 1; row <= numberOfRows; row++) 
+            {
+                for (int column = 1; column <= 10;  column++) 
+                {
+                    int x = startX + (column - 1) * (BAG_HEIGHT * BAG_WIDTH + GAP);
+                    int y = startY + (row - 1) * (BAG_HEIGHT * BAG_WIDTH + GAP);
+
+
+
+                        
+
+        }
+    }
+}
